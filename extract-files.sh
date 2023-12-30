b31ea736cad02f58e03a26a6831d6d52c267a3ec
rk134 <rahul@aospa.co>
2023-12-30 14:24:37 +0000

    no

--- extract-files.sh
+++ extract-files.sh
@@ -57,12 +57,25 @@ function blob_fixup() {
     case "${1}" in
         odm/etc/camera/CameraHWConfiguration.config)
             sed -i "/SystemCamera = / s/1;/0;/g" "${2}"
+            sed -i "/SystemCamera = / s/0;$/1;/" "${2}"
+            ;;
+        odm/etc/init/vendor-oplus-hardware-oplusSensor-V1-service.rc)
+            sed -i "/user/ s/system/root/g" "${2}"
+            ;;
+        product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml)
+            sed -i "s/\/my_product/\/product/" "${2}"
+            ;;
+        system_ext/lib64/libwfdnative.so)
+            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
+            ;;
+        vendor/etc/media_*/video_system_specs.json)
+            sed -i "/max_retry_alloc_output_timeout/ s/1000/0/" "${2}"
+            ;;
+        vendor/etc/msm_irqbalance.conf)
+            sed -i "s/IGNORED_IRQ=27,23,38$/&,115,332/" "${2}"
             ;;
         vendor/lib/libgui1_vendor.so)
             "${PATCHELF}" --replace-needed "libui.so" "libui-v30.so" "${2}"
-            ;;
-        vendor/lib64/hw/com.qti.chi.override.so)
-            "${SIGSCAN}" -p "45 B8 05 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "23 0B 00 94" -P "1F 20 03 D5" -f "${2}"
